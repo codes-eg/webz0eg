c3a706170979d6e8f22ee45eff32f2a95de068c5
codeseg <68105267+codes-eg@users.noreply.github.com>
2023-11-26 23:46:43 +0300

    Add files via upload

---
+++ coffee-3/script.js
@@ -0,0 +1,8 @@
+function signupformonoff() {
+  const signupform = document.getElementById("signupform");
+  if (signupform.style.display == "none") {
+    signupform.style.display = "block";
+  } else {
+    signupform.style.display = "none";
+  }
+}
